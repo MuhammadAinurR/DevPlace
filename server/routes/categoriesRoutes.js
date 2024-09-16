f0106614239bda32f0bd94a77d0089265d4be711
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-16 17:56:17 +0700

    add submodules

--- server/routes/categoriesRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getCategories } = require('../controllers/category');
-const categoriesRouter = express.Router();
-
-categoriesRouter.get('/', getCategories);
-
-module.exports = categoriesRouter;
